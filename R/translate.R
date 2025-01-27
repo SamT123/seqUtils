537f343faf00c683bbd38ae901db5e26d4fd0f85
SamT123 <samturner0306@gmail.com>
2025-01-27 16:37:18 +0000

    Add option to align to reference in translate()
    Allows deletions to be handled

--- R/translate.R
+++ R/translate.R
@@ -1,12 +1,25 @@
-#' Wrapper for Biostrings::translate so a character vector can be provided
+#' Wrapper for Biostrings::translate
 #'
-#' @param sequences character vector of DNA/RNA sequences
+#' Accepts character vector, and handles deletions
+#'
+#' @param sequences Character vector of DNA/RNA sequences
+#' @param reference (optional) Sequences are aligned to `reference` if provided. Useful if, for example, there are deletions in sequences
 #'
 #' @return character vector of AA sequences, with names preserved
 #' @importFrom Biostrings translate DNAStringSet
+#' @importFrom stringr str_detect str_remove_all
 #'
 #' @export
-translate = function(sequences){
+translate = function(sequences, reference_aas = NULL){
+
+  any_deletions = any(stringr::str_detect(sequences, "-"))
+
+  if (any_deletions & is.null(reference_aas)){
+    warning("Deletions detected & no reference for alignment provided. Output will be unaligned.")
+  }
+
+  sequences = stringr::str_remove_all(sequences, "-")
+
   aa_sequences = Biostrings::translate(
     Biostrings::DNAStringSet(sequences),
     if.fuzzy.codon = "X"
@@ -14,6 +27,10 @@ translate = function(sequences){
 
   aa_sequences = as.character(aa_sequences)
   names(aa_sequences) = names(sequences)
+
+  if (!is.null(reference_aas)){
+    aa_sequences = mafft_align(aa_sequences, reference_aas)
+  }
 
   aa_sequences
 }
